8734d18a94ca9f7379308e5724a06654ea8b016f
MarcoBenelli <marco.benelli@stud.unifi.it>
2021-01-18 15:51:29 +0100

    Commit test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Point.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Point p1({1, 2});
+    Point p2({1, 2});
+    p1 += p2;
+
     return 0;
 }
